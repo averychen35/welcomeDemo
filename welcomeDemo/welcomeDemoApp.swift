c179359df24f685518aebf791e07b26190f55530
abosflutely <achen315@student.fuhsd.org>
2023-09-12 14:20:17 -0700

    Initial Commit

---
+++ welcomeDemo/welcomeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  welcomeDemoApp.swift
+//  welcomeDemo
+//
+//  Created by Avery Chen on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct welcomeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
